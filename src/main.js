87b762804bfbdd31a99be6547fa6e87bfae1ff70
Nur Seda Ağgünlü <nursedaaggunlu@gmail.com>
2026-02-02 19:02:32 +0300

    -My Library- completed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import './css/styles.css';
-//import { initHeader } from './js/header';
+import './css/header.css';
+import './css/my-library.css';
+import { initHeader } from './js/header';
+import { initMyLibrary } from './js/my-library';
 
-//document.addEventListener('DOMContentLoaded', () => {
-//  initHeader();
-//});
+document.addEventListener('DOMContentLoaded', () => {
+  initHeader();
+  initMyLibrary();
+});
+
 
