af32d4551d35674a5180c5d6c2cf312f98d9e6e1
Cigdem Ergal <cigdemtahtasiz@gmail.com>
2026-02-11 14:23:44 +0300

    Fix GitHub Pages asset/base paths

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
-    base: './',
+    base: command === 'serve' ? '/' : '/cinemania/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
